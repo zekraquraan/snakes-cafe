99e4b399dec052cc0735ff834641a445fae75ebb
zekraquraan <zekraquraan7@gmail.com>
2023-05-08 14:04:22 +0300

    2:04

--- snackes-cafe/snackes-cafe.py
+++ snackes-cafe/snackes-cafe.py
@@ -1,58 +1,67 @@
+menu = {
+    "Appetizers": ["Wings", "Cookies", "Spring Rolls"],
+    "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"],
+    "Desserts": ["Ice Cream", "Cake", "Pie"],
+    "Drinks": ["Coffee", "Tea", "Unicorn Tears"]
+}
 
-menu={
-    "Appetizers":["Wings","Cookies","Spring Rolls"],
-    "Entrees":["Salamon","Steak","Meat Tornado","A Literal Garden"],
-    "Desserts":["Ice Cream","Cake","Pie"],
-    "Drinks":["Coffee","Tea","Unicorn Tears"]
-   }
+
+def intro():
+    print(
+        '''
+        **************************************
+        **    Welcome to the Snakes Cafe!   **
+        **    Please see our menu below.    **
+        **
+        ** To quit at any time, type "quit" **
+        **************************************
+        '''
+    )
+    print_menu()
 
 
-# FUNCTION
+def print_menu():
+    print("\nMenu")
+    print("----------")
+    for category, items in menu.items():
+        print(category)
+        for item in items:
+            print(item)
 
-def into():
-   print(
-    '''
-    **************************************
-    **    Welcome to the Snakes Cafe!   **
-    **    Please see our menu below.    **
-    **
-    ** To quit at any time, type "quit" **
-    **************************************
-    '''
-   )
 
-def order():
-    
-    order=input("What would you like to order? \n >")
-    return order
+def take_order():
+    order_count = {}
 
-def end():
-   print("Thanks for visiting !!")
-    
-def output():
-    userInput = order()
-    count = 0
-    while userInput.lower() != "quit":
-        for x in menu:
-            for item in menu[x]:
-                if userInput.lower() == item.lower():
-                    count += 1
-                    print(count , "order of" ,item, "has been added to your meal")
-                    break
-            else:
-                continue
+    while True:
+        order = input("What would you like to order? \n> ")
+
+        if order.lower() == "quit":
             break
-        else:
+
+        found_item = False
+
+        for category, items in menu.items():
+            for item in items:
+                if order.lower() == item.lower():
+                    found_item = True
+                    order_count[item] = order_count.get(item, 0) + 1
+                    count = order_count[item]
+                    print(f"{count} order{'s' if count > 1 else ''} of {item} has been added to your meal.")
+                    break
+
+            if found_item:
+                break
+
+        if not found_item:
             print("Sorry, we don't have that item on the menu. Please, select from the menu.")
-        userInput = order()
-    end()        
-    
-             
 
-into() 
-output()
+    print("Thanks for visiting!!")
 
 
+def run_snakes_cafe():
+    intro()
+    take_order()
 
-    
-    
+
+if __name__ == "__main__":
+    run_snakes_cafe()
